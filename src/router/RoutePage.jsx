a4e5ef1c70ba907ebe1257087b2c1a1d48a26487
s0ojin <onepst@hanyang.ac.kr>
2024-01-17 17:35:13 +0900

    FEAT: protected route기능 추가

--- src/router/RoutePage.jsx
+++ src/router/RoutePage.jsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import PrivateRoute from "./PrivateRoute";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
 import Home from "../pages/Home";
@@ -7,10 +8,17 @@ import Mypage from "../pages/Mypage";
 export default function RoutePage() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/mypage" element={<Mypage />} />
+      {/* 로그인 무관 */}
+      {/* 로그인 필수 */}
+      <Route element={<PrivateRoute authentication={true} />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/mypage" element={<Mypage />} />
+      </Route>
+      {/* 로그아웃 필수 */}
+      <Route element={<PrivateRoute authentication={false} />}>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Route>
     </Routes>
   );
 }
