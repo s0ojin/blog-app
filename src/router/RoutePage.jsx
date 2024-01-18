5d115d0c664436e2df5c5f0688e79f6f46a65619
s0ojin <onepst@hanyang.ac.kr>
2024-01-18 23:08:22 +0900

    FEAT: 글 상세 페이지

--- src/router/RoutePage.jsx
+++ src/router/RoutePage.jsx
@@ -5,6 +5,7 @@ import Signup from "../pages/Signup";
 import Home from "../pages/Home";
 import Mypage from "../pages/Mypage";
 import CreatePost from "../pages/CreatePost";
+import PostDetail from "../pages/PostDetail";
 
 export default function RoutePage() {
   return (
@@ -15,6 +16,7 @@ export default function RoutePage() {
         <Route path="/" element={<Home />} />
         <Route path="/mypage" element={<Mypage />} />
         <Route path="/post/create" element={<CreatePost />} />
+        <Route path="/post/:postId" element={<PostDetail />} />
       </Route>
       {/* 로그아웃 필수 */}
       <Route element={<PrivateRoute authentication={false} />}>
