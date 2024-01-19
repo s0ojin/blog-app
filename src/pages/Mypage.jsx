bf63fb93677b91451c4c27adb9d7e8d5e36fc822
s0ojin <onepst@hanyang.ac.kr>
2024-01-19 14:00:57 +0900

    FEAT: 글 조회 카테고리 추가

--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -1,3 +1,18 @@
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase";
+
 export default function Mypage() {
-  return <div>마이페이지임당</div>;
+  const logoutHandler = () => {
+    signOut(auth)
+      .then(() => {
+        alert("로그아웃이 완료되었습니다.");
+      })
+      .catch((err) => alert(err));
+  };
+
+  return (
+    <div>
+      <button onClick={logoutHandler}>로그아웃</button>
+    </div>
+  );
 }
